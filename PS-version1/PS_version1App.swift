05a6d96a327df1cc27856bab6f3e0d052d55a9e7
natruz <88256962+natruz@users.noreply.github.com>
2022-07-18 20:44:08 +0800

    Initial Commit

---
+++ PS-version1/PS_version1App.swift
@@ -0,0 +1,17 @@
+//
+//  PS_version1App.swift
+//  PS-version1
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 18/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PS_version1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
